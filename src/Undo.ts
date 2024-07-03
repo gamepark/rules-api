06264e5ba930e32d5e31c2efbec80b4544737aea
Romain Fromi <romain.fromi@gmail.com>
2024-07-03 13:46:30 +0200

    Use undo to close help dialogs and remove restoreLocalMoves from undo API (#4)

--- src/Undo.ts
+++ src/Undo.ts
@@ -1,11 +1,10 @@
 import { Action } from './Action'
 
-export interface Undo<Game, Move = string, PlayerId = number> {
+export interface Undo<Move = string, PlayerId = number> {
   canUndo(action: Action<Move, PlayerId>, consecutiveActions: Action<Move, PlayerId>[]): boolean
-  restoreLocalMoves?(localState: Game): void
 }
 
-export function hasUndo<Game, Move, PlayerId>(rules: Object): rules is Undo<Game, Move, PlayerId> {
-  const test = rules as Undo<Game, Move, PlayerId>
+export function hasUndo<Move, PlayerId>(rules: Object): rules is Undo<Move, PlayerId> {
+  const test = rules as Undo<Move, PlayerId>
   return typeof test.canUndo === 'function'
 }
--- src/material/MaterialRules.ts
+++ src/material/MaterialRules.ts
@@ -31,7 +31,7 @@ import { MaterialRulesPart, MaterialRulesPartCreator } from './rules'
 export abstract class MaterialRules<Player extends number = number, MaterialType extends number = number, LocationType extends number = number>
   extends Rules<MaterialGame<Player, MaterialType, LocationType>, MaterialMove<Player, MaterialType, LocationType>, Player>
   implements RandomMove<MaterialMove<Player, MaterialType, LocationType>, MaterialMoveRandomized<Player, MaterialType, LocationType>>,
-    Undo<MaterialGame<Player, MaterialType, LocationType>, MaterialMove<Player, MaterialType, LocationType>, Player> {
+    Undo<MaterialMove<Player, MaterialType, LocationType>, Player> {
 
   material(type: MaterialType): Material<Player, MaterialType, LocationType> {
     return new Material(type, Array.from((this.game.items[type] ?? []).entries()).filter(entry => entry[1].quantity !== 0))
@@ -147,9 +147,6 @@ export abstract class MaterialRules<Player extends number = number, MaterialType
           case LocalMoveType.DisplayHelp:
             this.game.helpDisplay = move.helpDisplay
             break
-          case LocalMoveType.CloseHelpDisplay:
-            delete this.game.helpDisplay
-            break
           case LocalMoveType.DropItem:
             this.game.droppedItem = move.item
             break
@@ -241,10 +238,6 @@ export abstract class MaterialRules<Player extends number = number, MaterialType
 
   protected isRandomMove(move: MaterialMove<Player, MaterialType, LocationType>): boolean {
     return isShuffle(move) || isRoll(move)
-  }
-
-  restoreLocalMoves(game: MaterialGame<Player, MaterialType, LocationType>) {
-    this.game.helpDisplay = game.helpDisplay
   }
 
   isOver(): boolean {
