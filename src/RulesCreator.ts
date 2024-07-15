dfbf6017e2975f90aa3cdfba249f9d9e12990785
Romain Fromi <romain.fromi@gmail.com>
2024-07-15 17:48:49 +0200

    Remove DefaultGameSetup (retro-compatibility is now assumed on GamePark side) and simplify RuleCreator.ts

--- src/RulesCreator.ts
+++ src/RulesCreator.ts
@@ -1,7 +1,5 @@
 import { Rules } from './Rules'
 
-export interface RulesCreator<Game = any, Move = any, Player = number, Options = any> {
+export interface RulesCreator<Game = any, Move = any, Player = number> {
   new(state: Game, client?: { player?: Player }): Rules<Game, Move, Player>
-
-  new(options: Options): Rules<Game, Move, Player> // TODO: we should be able to remove this construction now that GameSetup & Rules are distinct classes
 }
--- src/setup/index.ts
+++ src/setup/index.ts
@@ -1,3 +1,2 @@
-export * from './DefaultGameSetup'
 export * from './GameSetup'
 export * from './GameSetupCreator'
