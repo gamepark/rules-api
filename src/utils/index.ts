32e637fe4cc6564f7bbe1be6439d1979fa3a39f1
Romain Fromi <romain.fromi@gmail.com>
2024-07-15 17:33:05 +0200

    Move loopWithFuse and applyAutomaticMoves function in utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
+export * from './automatic-moves.util'
 export * from './enum.util'
 export * from './grid.util'
 export * from './grid.squares.util'
 export * from './listing.util'
+export * from './loops.util'
