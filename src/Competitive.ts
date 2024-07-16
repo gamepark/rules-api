83e6fa244cd0f410d6b29047db29cd307b271f3a
Romain Fromi <romain.fromi@gmail.com>
2024-07-16 08:56:41 +0200

    Move rankPlayers utility function in a rank.util file

--- src/Competitive.ts
+++ src/Competitive.ts
@@ -23,31 +23,3 @@ export function isCompetitiveScore<Game, Move, PlayerId>(rules: Rules<any, any, 
 export function isCompetitiveRank<Game, Move, PlayerId>(rules: Rules<any, any, PlayerId>): rules is CompetitiveRank<Game, Move, PlayerId> {
   return typeof (rules as CompetitiveRank<Game, Move, PlayerId>).rankPlayers === 'function'
 }
-
-export const rankPlayers = <PlayerId = number>(rules: Rules<any, any, PlayerId>, playerA: PlayerId, playerB: PlayerId) => {
-  if (isCompetitiveRank(rules)) {
-    return rules.rankPlayers(playerA, playerB)
-  }
-  if (isCompetitiveScore(rules)) {
-    const scoreA = rules.getScore(playerA)
-    const scoreB = rules.getScore(playerB)
-    if (scoreA !== scoreB) {
-      return scoreB - scoreA
-    }
-    if (rules.getTieBreaker) {
-      for (let tieBreaker = 1; tieBreaker <= 10; tieBreaker++) {
-        const tieBreakerA = rules.getTieBreaker(tieBreaker, playerA)
-        const tieBreakerB = rules.getTieBreaker(tieBreaker, playerB)
-        if (tieBreakerA === undefined) {
-          return tieBreakerB === undefined ? 0 : Infinity
-        } else if (tieBreakerB === undefined) {
-          return -Infinity
-        } else if (tieBreakerA !== tieBreakerB) {
-          return tieBreakerB - tieBreakerA
-        }
-      }
-      console.error('getTieBreaker must return "undefined" after the last tie breaker is still a tie!')
-    }
-  }
-  return 0
-}
---
+++ src/utils/rank.util.ts
@@ -0,0 +1,30 @@
+import { isCompetitiveRank, isCompetitiveScore } from '../Competitive'
+import { Rules } from '../Rules'
+
+export const rankPlayers = <PlayerId = any>(rules: Rules<any, any, PlayerId>, playerA: PlayerId, playerB: PlayerId) => {
+  if (isCompetitiveRank(rules)) {
+    return rules.rankPlayers(playerA, playerB)
+  }
+  if (isCompetitiveScore(rules)) {
+    const scoreA = rules.getScore(playerA)
+    const scoreB = rules.getScore(playerB)
+    if (scoreA !== scoreB) {
+      return scoreB - scoreA
+    }
+    if (rules.getTieBreaker) {
+      for (let tieBreaker = 1; tieBreaker <= 10; tieBreaker++) {
+        const tieBreakerA = rules.getTieBreaker(tieBreaker, playerA)
+        const tieBreakerB = rules.getTieBreaker(tieBreaker, playerB)
+        if (tieBreakerA === undefined) {
+          return tieBreakerB === undefined ? 0 : Infinity
+        } else if (tieBreakerB === undefined) {
+          return -Infinity
+        } else if (tieBreakerA !== tieBreakerB) {
+          return tieBreakerB - tieBreakerA
+        }
+      }
+      console.error('getTieBreaker must return "undefined" after the last tie breaker is still a tie!')
+    }
+  }
+  return 0
+}
