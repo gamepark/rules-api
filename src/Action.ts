d32aff814e5f280a9ee117d18d669e922e5e6e57
Romain Fromi <romain.fromi@gmail.com>
2024-07-16 15:38:48 +0200

    TSDoc for Action.ts

--- src/Action.ts
+++ src/Action.ts
@@ -1,3 +1,10 @@
+/**
+ * An action is a move played by a player, with the consequences that resulted from it.
+ * @property id Unique identifier for the action (see {@link Undo})
+ * @property playerId Identifier of the player which played the move at the origin of the action
+ * @property move Move at the origin of the action
+ * @property consequences All the move that automatically happened after the move was played
+ */
 export type Action<Move = any, PlayerId = any> = {
   id?: string
   playerId: PlayerId
