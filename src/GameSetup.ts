7a1e419a401ea7e02e36610b2684d7f219848238
Romain Fromi <romain.fromi@gmail.com>
2024-07-16 16:20:28 +0200

    TSDoc for GameSetup.ts

--- src/GameSetup.ts
+++ src/GameSetup.ts
@@ -1,7 +1,21 @@
+/**
+ * Each game must provide the initial game state when a new game is created.
+ * Create a class that implements this interface to provide it.
+ * The constructor must not have any arguments (see {@link GameSetupCreator}).
+ */
 export interface GameSetup<Game = any, Options = any> {
+  /**
+   * Create a new game base on the options chosen by the players
+   *
+   * @param options Options of the game (see {@link OptionsSpec})
+   * @returns the initial game state
+   */
   setup(options: Options): Game
 }
 
+/**
+ * Creator interface for {@link GameSetup}.
+ */
 export interface GameSetupCreator<Game = any, Options = any> {
   new(): GameSetup<Game, Options>
 }
