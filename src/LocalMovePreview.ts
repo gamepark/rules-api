1d821ff30da884fc5aaa3aad1c7aaa5f2c36b7b9
Romain Fromi <romain.fromi@gmail.com>
2024-07-17 12:05:35 +0200

    TSDoc for LocalMovePreview.ts

--- src/LocalMovePreview.ts
+++ src/LocalMovePreview.ts
@@ -1,7 +1,21 @@
+/**
+ * This interface allows to keep a move played only on the player's side.
+ * It is convenient to allow the player to "preview" the results of their action before validating it.
+ */
 export interface LocalMovePreview<Move = any> {
+  /**
+   * Function called on client side after a move is played, before it is sent to the server
+   * @param move The move that was played
+   * @return true if the move must not be sent to the server
+   */
   previewMove(move: Move): boolean
 }
 
+/**
+ * Type guard for {@link LocalMovePreview} interface
+ * @param rules The game's rules
+ * @return true if the rules implements {@link LocalMovePreview}
+ */
 export function hasLocalMovePreview<Move = any>(rules: Object): rules is LocalMovePreview<Move> {
   return typeof (rules as LocalMovePreview<Move>).previewMove === 'function'
 }
