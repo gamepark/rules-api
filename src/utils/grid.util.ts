497a0ea20735a73484dcefb220e878625bb2a17c
Romain Fromi <romain.fromi@gmail.com>
2024-07-18 10:34:53 +0200

    TSDoc for grid utils

--- src/utils/grid.util.ts
+++ src/utils/grid.util.ts
@@ -1,8 +1,16 @@
+/**
+ * Type for any object with XY number coordinates
+ */
 export type XYCoordinates = {
   x: number
   y: number
 }
 
+/**
+ * Type guard to know if we have an object with XY coordinates
+ * @param coordinates The object
+ * @returns true if coordinates is a {@link XYCoordinates}
+ */
 export const isXYCoordinates = (coordinates: any): coordinates is XYCoordinates => {
   return typeof coordinates === 'object' && typeof coordinates.x === 'number' && typeof coordinates.y === 'number'
 }
