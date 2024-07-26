d513faf2b4bcf4c555eeb3aef5317045949486af
Romain Fromi <romain.fromi@gmail.com>
2024-07-26 10:05:46 +0200

    TSDoc for Location.ts

--- src/material/items/MaterialItem.ts
+++ src/material/items/MaterialItem.ts
@@ -7,7 +7,7 @@ import { Location } from '../location'
  * @typeparam L - Numeric enum of the types of location in the game where the material can be located
  * @typeparam Id - Type of id property of the item. Must be JSON serializable.
  *
- * @property id Item identifier. Meant to identify the item, but not in a unique way. For example, two identical cards in a game should have the same id.
+ * @property id Optional item identifier. Meant to identify the item, but not in a unique way. For example, two identical cards in a game should have the same id.
  * @property quantity Quantity of the item. An item without a quantity will be considered to have a quantity of 1.
  * @property {Location} location Location of the item in the game (in a player's hand, on the table...). See {@link Location}.
  * @property {number | boolean | undefined} selected Flag the item if selected. Items with a quantity can be partially selected using a number of select items.
--- src/material/location/Location.ts
+++ src/material/location/Location.ts
@@ -1,5 +1,21 @@
 import { XYCoordinates } from '../../utils'
 
+/**
+ * A location where to place the material in a game
+ *
+ * @typeparam Player - identifier of a player. Either a number or a numeric enum (eg: PlayerColor)
+ * @typeparam LocationType - Numeric enum of the types of location in the game where the material can be located
+ * @typeparam Id - Type of id property of the location. Must be JSON serializable.
+ *
+ * @property type The type of location. Usually an enum named LocationType.
+ * @property id Optional location identifier, if the coordinates, player and parent items are not enough.
+ * @property player Optional owner player of that location.
+ * @property parent Optional index of the parent item this location belongs to.
+ * @property rotation Rotation applied to the item's location
+ * @property x Optional X-coordinate
+ * @property y Optional Y-coordinate
+ * @property z Optional Z-coordinate
+ */
 export type Location<Player = number, LocationType = number, Id = any, Rotation = any> = {
   type: LocationType
   id?: Id
@@ -8,8 +24,20 @@ export type Location<Player = number, LocationType = number, Id = any, Rotation 
   rotation?: Rotation
 } & Partial<Coordinates>
 
+/**
+ * A 3-dimension coordinates system
+ *
+ * @property x X-coordinate
+ * @property y Y-coordinate
+ * @property z Z-coordinate
+ */
 export type Coordinates = XYCoordinates & { z: number }
 
+/**
+ * Utility function to know whether some locations share the same type, id, player and parent.
+ * @param locations Locations to test
+ * @returns true if all the locations belongs to the same "area" (only varies in coordinates or rotation)
+ */
 export const isSameLocationArea = (...locations: Partial<Location>[]): boolean => locations.every((l, _, [l0]) =>
   l.type === l0.type && l.id === l0.id && l.player === l0.player && l.parent === l0.parent
 )
