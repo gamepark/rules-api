c2e896e62e66f6de543c41363e192290aec24930
Romain Fromi <romain.fromi@gmail.com>
2024-07-27 18:38:09 +0200

    TSDoc for GameMemory.ts and PlayerMemory.ts

--- src/material/memory/GameMemory.ts
+++ src/material/memory/GameMemory.ts
@@ -1,9 +1,21 @@
 import { MaterialGame } from '../MaterialGame'
 
+/**
+ * Utility class to manipulate the memory in a {@link MaterialGame}.
+ */
 export class GameMemory<Player extends number = number> {
+  /**
+   * @constructor
+   * @param {MaterialGame} game Current state of the game
+   */
   constructor(private game: MaterialGame<Player>) {
   }
 
+  /**
+   * Save a new value inside the memory
+   * @param key The key to index the memorized value
+   * @param value Any JSON serializable value to store, or a function that takes previous stored value and returns the new value to store
+   */
   memorize<T = any>(key: keyof any, value: T | ((lastValue: T) => T)): void {
     if (typeof value === 'function') {
       this.game.memory[key] = (value as (lastValue: T) => T)(this.game.memory[key])
@@ -15,10 +27,19 @@ export class GameMemory<Player extends number = number> {
     }
   }
 
+  /**
+   * Get a value stored in the memory
+   * @param key The key to index the memorized value
+   * @returns the value stored (or undefined is nothing was stored under this key yet)
+   */
   remind<T = any>(key: keyof any): T {
     return this.game.memory[key]
   }
 
+  /**
+   * Delete a value from the memory
+   * @param key Key of the value to delete
+   */
   forget(key: keyof any): void {
     delete this.game.memory[key]
   }
--- src/material/memory/PlayerMemory.ts
+++ src/material/memory/PlayerMemory.ts
@@ -1,9 +1,22 @@
 import { MaterialGame } from '../MaterialGame'
 
+/**
+ * Utility class to manipulate a player's memory in a {@link MaterialGame}.
+ */
 export class PlayerMemory<Player extends number = number> {
+  /**
+   * @constructor
+   * @param {MaterialGame} game Current state of the game
+   * @param player The player to work with
+   */
   constructor(private game: MaterialGame<Player>, private player: Player) {
   }
 
+  /**
+   * Save a new value inside the memory
+   * @param key The key to index the memorized value
+   * @param value Any JSON serializable value to store, or a function that takes previous stored value and returns the new value to store
+   */
   memorize<T = any>(key: keyof any, value: T | ((lastValue: T) => T)): void {
     if (!this.game.memory[key]) this.game.memory[key] = {}
     if (typeof value === 'function') {
@@ -16,10 +29,19 @@ export class PlayerMemory<Player extends number = number> {
     }
   }
 
+  /**
+   * Get a value stored in the memory
+   * @param key The key to index the memorized value
+   * @returns the value stored (or undefined is nothing was stored under this key yet)
+   */
   remind<T = any>(key: keyof any): T {
     return this.game.memory[key]?.[this.player]
   }
 
+  /**
+   * Delete a value from the memory
+   * @param key Key of the value to delete
+   */
   forget(key: keyof any): void {
     delete this.game.memory[key]?.[this.player]
   }
