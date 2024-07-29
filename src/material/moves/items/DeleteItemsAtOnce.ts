1443c8cb06e2dd1a7b008ebf274545872ea690ec
Romain Fromi <romain.fromi@gmail.com>
2024-07-29 11:22:38 +0200

    TSDoc for DeleteItemsAtOnce.ts

--- src/material/moves/items/DeleteItemsAtOnce.ts
+++ src/material/moves/items/DeleteItemsAtOnce.ts
@@ -3,15 +3,32 @@ import { MoveKind } from '../MoveKind'
 import { MaterialMoveCommon } from './ItemMove'
 import { ItemMoveType } from './ItemMoveType'
 
+/**
+ * Move object that will delete multiple {@link MaterialItem} when played.
+ * Use {@link Material} utility to build the move easily.
+ * Use this when you need to delete a lot of items without animating it one by one, to reduce the data payload.
+ *
+ * @property indexes indexes of the items to delete
+ */
 export type DeleteItemsAtOnce<MaterialType extends number = number> = MaterialMoveCommon<MaterialType> & {
   type: typeof ItemMoveType.DeleteAtOnce
   indexes: number[]
 }
 
+/**
+ * Type guard to test if a {@link MaterialMove} is a {@link DeleteItemsAtOnce} move
+ * @param move Move to test
+ * @returns true if move is a {@link DeleteItemsAtOnce}
+ */
 export function isDeleteItemsAtOnce<P extends number, M extends number, L extends number>(move: MaterialMove<P, M, L>): move is DeleteItemsAtOnce<M> {
   return move.kind === MoveKind.ItemMove && move.type === ItemMoveType.DeleteAtOnce
 }
 
+/**
+ * Function to get a type guard for a {@link DeleteItemsAtOnce} move for specific item types.
+ * @param type Item type to test
+ * @returns a type guard similar as {@link isDeleteItemsAtOnce} but that also verify the item type.
+ */
 export function isDeleteItemTypeAtOnce<P extends number, M extends number, L extends number>(
   type: M
 ): (move: MaterialMove<P, M, L>) => move is DeleteItemsAtOnce<M> {
