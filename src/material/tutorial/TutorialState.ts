6f54f22d1006073f638e9785d7471d4d3a19fd17
Romain Fromi <romain.fromi@gmail.com>
2024-07-29 14:16:25 +0200

    TSDoc for TutorialState.ts

--- src/material/tutorial/TutorialState.ts
+++ src/material/tutorial/TutorialState.ts
@@ -1,5 +1,12 @@
 import { MaterialMove } from '../moves'
 
+/**
+ * Data structure for the state of the tutorial in a tutorial game implement with {@link MaterialRules}.
+ * @property step Current step of the tutorial.
+ * @property popupClosed true when the popup has been closed by the player.
+ * @property stepComplete if true, the tutorial engine will move on to next step automatically.
+ * @property interrupt The automatic consequences that have been interrupted to display some information explaining what is happening.
+ */
 export type TutorialState<P extends number = number,  M extends number = number,  L extends number = number> = {
   step: number
   popupClosed: boolean
