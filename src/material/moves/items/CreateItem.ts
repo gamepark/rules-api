64d3459bc987e12f3c20c4c01cc5b2d060dacd0a
Romain Fromi <romain.fromi@gmail.com>
2024-07-29 11:15:16 +0200

    TSDoc for CreateItem.ts

--- src/material/moves/items/CreateItem.ts
+++ src/material/moves/items/CreateItem.ts
@@ -4,15 +4,28 @@ import { MaterialMoveCommon } from './ItemMove'
 import { MaterialMove } from '../MaterialMove'
 import { MoveKind } from '../MoveKind'
 
+/**
+ * Move object that will create one {@link MaterialItem} when played
+ */
 export type CreateItem<P extends number = number, M extends number = number, L extends number = number> = MaterialMoveCommon<M> & {
   type: typeof ItemMoveType.Create
   item: MaterialItem<P, L>
 }
 
+/**
+ * Type guard to test if a {@link MaterialMove} is a {@link CreateItem} move
+ * @param move Move to test
+ * @returns true if move is a {@link CreateItem}
+ */
 export function isCreateItem<P extends number, M extends number, L extends number>(move: MaterialMove<P, M, L>): move is CreateItem<P, M, L> {
   return move.kind === MoveKind.ItemMove && move.type === ItemMoveType.Create
 }
 
+/**
+ * Function to get a type guard for a {@link CreateItem} move for specific item types.
+ * @param type Item type to test
+ * @returns a type guard similar as {@link isCreateItem} but that also verify the item type.
+ */
 export function isCreateItemType<P extends number, M extends number, L extends number>(
   type: M
 ): (move: MaterialMove<P, M, L>) => move is CreateItem<P, M, L> {
