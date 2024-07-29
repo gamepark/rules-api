beb0202b035323a5712a1bfab19d2118f32031d5
Romain Fromi <romain.fromi@gmail.com>
2024-07-29 14:12:40 +0200

    TSDoc for SimultaneousRule.ts

--- src/material/rules/SimultaneousRule.ts
+++ src/material/rules/SimultaneousRule.ts
@@ -2,28 +2,53 @@ import { PlayMoveContext } from '../../Rules'
 import { EndPlayerTurn, MaterialMove, MaterialMoveBuilder } from '../moves'
 import { MaterialRulesPart } from './MaterialRulesPart'
 
+/**
+ * Base class for any part of the rules where multiple players have to do something at the same time.
+ */
 export abstract class SimultaneousRule<Player extends number = number, MaterialType extends number = number, LocationType extends number = number>
   extends MaterialRulesPart<Player, MaterialType, LocationType> {
 
   endPlayerTurn = MaterialMoveBuilder.endPlayerTurn
 
+  /**
+   * See {@link Rules.isTurnToPlay}
+   */
   isTurnToPlay(player: Player): boolean {
     return this.game.rule?.players?.includes(player) ?? false
   }
 
+  /**
+   * See {@link Rules.getLegalMoves}
+   */
   getLegalMoves(player: Player): MaterialMove<Player, MaterialType, LocationType>[] {
     return this.isTurnToPlay(player) ? this.getActivePlayerLegalMoves(player) : []
   }
 
   abstract getActivePlayerLegalMoves(playerId: Player): MaterialMove<Player, MaterialType, LocationType>[]
 
+  /**
+   * This function is called immediately after a {@link EndPlayerTurn} has been played.
+   * @param _move The move which has just been played
+   * @param _context Context of execution
+   * @returns {MaterialMove[]} Any consequences that should automatically be played after the move
+   */
   onPlayerTurnEnd(_move: EndPlayerTurn<Player>, _context?: PlayMoveContext): MaterialMove<Player, MaterialType, LocationType>[] {
     return []
   }
 
+  /**
+   * Implement this to return the consequences when all the players have completed their task.
+   * Usually, a new rule part should start.
+   * @returns the consequences when the last awaited player have played
+   */
   abstract getMovesAfterPlayersDone(): MaterialMove<Player, MaterialType, LocationType>[]
 }
 
+/**
+ * Type guard to know if a {@link MaterialRulesPart} is a {@link SimultaneousRule}
+ * @param rule The rule
+ * @returns true if the rule if a {@link SimultaneousRule}
+ */
 export function isSimultaneousRule<P extends number = number, M extends number = number, L extends number = number>(
   rule?: MaterialRulesPart<P, M, L>
 ): rule is SimultaneousRule<P, M, L> {
