ca889653af61206f63bf1d044e43158a80fcad4a
Romain Fromi <romain.fromi@gmail.com>
2024-07-29 09:13:22 +0200

    TSDoc for ItemMove.ts

--- src/material/moves/items/ItemMove.ts
+++ src/material/moves/items/ItemMove.ts
@@ -9,6 +9,9 @@ import { RollItem } from './RollItem'
 import { SelectItem } from './SelectItem'
 import { Shuffle, ShuffleRandomized } from './Shuffle'
 
+/**
+ * Common type for all the moves that have an impact on the game material items
+ */
 export type ItemMove<Player extends number = number, MaterialType extends number = number, LocationType extends number = number>
   = CreateItem<Player, MaterialType, LocationType>
   | CreateItemsAtOnce<Player, MaterialType, LocationType>
@@ -20,13 +23,22 @@ export type ItemMove<Player extends number = number, MaterialType extends number
   | RollItem<Player, MaterialType, LocationType>
   | SelectItem<MaterialType>
 
+/**
+ * Common properties for all the types of {@link ItemMove}
+ */
 export type MaterialMoveCommon<MaterialType extends number = number> = {
   kind: typeof MoveKind.ItemMove
   itemType: MaterialType
 }
 
+/**
+ * An {@link ItemMove} but after if has been randomized (see {@link RandomMove})
+ */
 export type ItemMoveRandomized<P extends number = number, M extends number = number, L extends number = number>
   = Exclude<ItemMove<P, M, L>, Shuffle<M>> | ShuffleRandomized<M>
 
+/**
+ * An {@link ItemMove} but after if has been transformed into what a player or a spectator can view (see {@link HiddenInformation})
+ */
 export type ItemMoveView<P extends number = number, M extends number = number, L extends number = number>
   = Exclude<ItemMoveRandomized<P, M, L>, ShuffleRandomized<M>> | Shuffle<M>
