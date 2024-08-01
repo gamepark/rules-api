02136ebf1ae34a8d2f5f5b88ff72792332c05d3a
Romain Fromi <romain.fromi@gmail.com>
2024-08-01 15:38:26 +0200

    Fix circular Material / MaterialDeck dependency issue

--- src/material/items/index.ts
+++ src/material/items/index.ts
@@ -1,4 +1,4 @@
+export * from './MaterialDeck' // Important: must be before Material import to prevent a circular dependency crash
 export * from './Material'
 export * from './MaterialItem'
 export * from './MaterialMutator'
-export * from './MaterialDeck'
