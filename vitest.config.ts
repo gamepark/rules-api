a922eedc26d40937799b1d37424540c997b61a07
Romain Fromi <romain.fromi@gmail.com>
2026-07-24 09:44:25 +0200

    Config for measuring code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'node'
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.test.ts', 'src/tests/**', 'src/**/index.ts']
+    }
   }
 })
